9530308d55ebd2ea0bb45da5c51fceb13f7105c2
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-13 16:48:06 -0500

    added a assertNotSameTest class and practiced with assertNotSame

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,12 +1,23 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
     @Test
-    public void test(){
+    public void assertEqualsTest(){
         String expectedFavoriteGame = "Dark Souls";
         String  actualFavoriteGame= "Dark Souls";
 
         assertEquals(expectedFavoriteGame, actualFavoriteGame);
+    }
+    @Test
+    public void assertNotSameTest(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
     }
 }
